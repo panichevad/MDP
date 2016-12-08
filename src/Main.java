d698be430c8c088b6756a0b41a2de00d18a67c8f
panichevad <panicheva.d@gmail.com>
2016-12-08 15:50:40 +0100

    Not stupid anymore

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 /**
  * Created by ZLO on 07.12.2016.
  */
+import java.util.*;
 import problemeDevant.*;
 
 public class Main {
     public static void main(String[] args) {
         ProblemeDevant pr = new ProblemeDevant();
         ValueIteration valIter = new ValueIteration(pr);
-        //valIter.faireUneIteration();
-        //valIter.qval.toStrings();
+
         valIter.fairePlusieursIterations(100);
         valIter.qval.toStrings();
-        //valIter.faireUneIteration();
-        //valIter.qval.toStrings();
+        Map<StateXYO, ActionOriente> politique = valIter.qval.getPolitique();
+        StateXYO etatDepart = new StateXYO(3,0, 2);
+        StateXYO etatArrivee;
+        ActionOriente action;
+        for (int i = 0; i < 50; i++) {
+                action = politique.get(etatDepart);
+                etatArrivee = pr.transition(etatDepart, action).tirage();
+                etatDepart = etatArrivee;
+                System.out.println("Politique " + action);
+
+        }
+
+
     }
 
 }
---
+++ src/problemeDevant/QLearning.java
@@ -0,0 +1,16 @@
+package problemeDevant;
+import java.util.Map;
+/**
+ * Created by ZLO on 08.12.2016.
+ */
+/**
+public class QLearning {
+    private ProblemeDevant pr;
+    private StateXYO etatDepart;
+    private Map<StateXYO,Map<ActionOriente,Integer> t;
+    private QValeur qval;
+    private double gamma;
+
+
+}
+*/
