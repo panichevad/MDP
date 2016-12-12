2fec955cfe033ea1c7782804121d64a6d2f5baf1
panichevad <panicheva.d@gmail.com>
2016-12-12 04:18:41 +0100

    Stupid again

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,26 @@ import problemeDevant.*;
 public class Main {
     public static void main(String[] args) {
         ProblemeDevant pr = new ProblemeDevant();
-
-        /**
         ValueIteration valIter = new ValueIteration(pr);
 
         valIter.fairePlusieursIterations(100);
         valIter.qval.toStrings();
         Map<StateXYO, ActionOriente> politique = valIter.qval.getPolitique();
-        /**
-         StateXYO etatDepart = new StateXYO(0,3, 2);
+
+        StateXYO etatDepart = new StateXYO(0,3, 2);
         StateXYO etatArrivee;
 
         ActionOriente action;
-        for (int i = 0; i < 50; i++) {
-                action = politique.get(etatDepart);
-                etatArrivee = pr.transition(etatDepart, action).tirage();
-                etatDepart = etatArrivee;
-                System.out.println("Politique " + action);
-
+        for (int i = 0; i < 20; i++) {
+            action = politique.get(etatDepart);
+            System.out.print(etatDepart + " -> ");
+            etatArrivee = pr.transition(etatDepart, action).tirage();
+            etatDepart = etatArrivee;
+            System.out.print(action + " -> ");
         }
-         */
 
         QLearning qL = new QLearning(pr);
-        qL.apprendre(1000);
+        qL.apprendre(5000000);
         qL.qval.toStrings();
-
     }
-
 }
